6271d200566320958f60c760a7d04deba8eba8f9
Giorgio Giannotta <jorge_upa@msn.com>
2021-05-16 12:36:57 +0200

    Initial Commit

---
+++ Analogic Clock/Analogic_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Analogic_ClockApp.swift
+//  Analogic Clock
+//
+//  Created by Jorge Giannotta on 16/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Analogic_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Analogic Clock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Analogic Clock
+//
+//  Created by Jorge Giannotta on 16/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
